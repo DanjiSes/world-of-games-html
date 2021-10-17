87dbf3a3147b49c0e2b5e2d218f62f3169d269f6
Danil Savchenko <danil.savchenko.dev@gmail.com>
2021-10-17 23:06:21 +0300

    My attemts for autoplay on mobile

--- build/lpFiles/wog/js/main.js
+++ build/lpFiles/wog/js/main.js
@@ -19,6 +19,36 @@ $.fn.is_on_screen = function () {
   );
 };
 
+var tag = document.createElement("script");
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+function onYouTubeIframeAPIReady() {
+  $("[data-youtube-viedo]").each(function () {
+    var $this = $(this);
+    var vidId = $this.attr("data-youtube-viedo");
+    var elId = $this.attr("id");
+
+    if (!elId) throw new Error("У элемента обязательно должен быть атрибут id");
+
+    function onPlayerReady(event) {
+      var newVideoObject = {};
+      newVideoObject[elId] = event.target;
+      window.wgYT = Object.assign(window.wgYT || {}, newVideoObject);
+    }
+
+    new YT.Player(elId, {
+      height: "360",
+      width: "640",
+      videoId: vidId,
+      events: {
+        onReady: onPlayerReady,
+      },
+    });
+  });
+}
+
 $(function () {
   $("[data-html-on-visible]").each(function () {
     var $this = $(this);
