17901766e3e94467e7be87bc6bd8faba838bfecf
Danil Savchenko <danil.savchenko.dev@gmail.com>
2021-10-18 00:39:01 +0300

    add super sounds slider without pain for client

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -49,6 +49,19 @@ function onYouTubeIframeAPIReady() {
   });
 }
 
+function chunkArray(array, size) {
+  let result = [];
+  for (value of array) {
+    let lastArray = result[result.length - 1];
+    if (!lastArray || lastArray.length == size) {
+      result.push([value]);
+    } else {
+      lastArray.push(value);
+    }
+  }
+  return result;
+}
+
 $(function () {
   $("[data-html-on-visible]").each(function () {
     var $this = $(this);
